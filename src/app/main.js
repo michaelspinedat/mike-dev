cf910c2c440dbed2662cc8fe334d06c33873d42a
Michael Steven Pineda Torres <stevenpnd@gmail.com>
2023-01-24 22:23:45 -0500

    Implement header and navbar

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,25 @@
-console.log('Hola mundo')
+// navbar
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+const navLink = document.querySelectorAll('.nav__link')
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+function linkAction() {
+  navMenu.classList.remove('show-menu')
+}
+
+if (navLink) {
+  navLink.forEach(link => link.addEventListener('click', linkAction))
+}
