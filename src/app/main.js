64b41ca492e3afecd38dd246c98dc67b9df2befe
Michael Steven Pineda Torres <stevenpnd@gmail.com>
2023-01-25 21:41:44 -0500

    Implement skills section

--- src/app/main.js
+++ src/app/main.js
@@ -2,8 +2,13 @@
 const navMenu = document.getElementById('nav-menu')
 const navToggle = document.getElementById('nav-toggle')
 const navClose = document.getElementById('nav-close')
-const navLink = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link')
 
+// skills
+const skillsContents = document.querySelectorAll('.skills__content')
+const skillsHeaders = document.querySelectorAll('.skills__header')
+
+// navbar logic
 if (navToggle) {
   navToggle.addEventListener('click', () => {
     navMenu.classList.add('show-menu')
@@ -20,6 +25,32 @@ function linkAction() {
   navMenu.classList.remove('show-menu')
 }
 
-if (navLink) {
-  navLink.forEach(link => link.addEventListener('click', linkAction))
+if (navLinks) {
+  navLinks.forEach(link => link.addEventListener('click', linkAction))
 }
+
+// skills logic
+function toggleSkill(skill) {
+  if (skill.classList.contains('skills__open')) {
+    skill.classList.remove('skills__open')
+    skill.classList.add('skills__close')
+  } else {
+    skill.classList.remove('skills__close')
+    skill.classList.add('skills__open')
+  }
+}
+
+function toggleSkills(e) {
+  const skillHeaderSelected = e.currentTarget
+
+  skillsContents.forEach(content => {
+    if (content == skillHeaderSelected.parentNode)
+      toggleSkill(content)
+    else {
+      content.classList.remove('skills__open')
+      content.classList.add('skills__close')
+    }
+  })
+}
+
+skillsHeaders.forEach(header => header.addEventListener('click', toggleSkills))
